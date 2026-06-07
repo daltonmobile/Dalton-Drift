00084f55469f199dfbe82ea2ef484cc3f1015572
daltonmobile <daltonpulse@gmail.com>
2026-06-07 06:49:12 +0200

    Delete js directory

--- js/utils.js
+++
@@ -1,9 +0,0 @@
-// utils.js
-const Utils = (() => {
-  const lerp      = (a, b, t) => a + (b - a) * t;
-  const clamp     = (v, lo, hi) => Math.max(lo, Math.min(hi, v));
-  const randRange = (a, b) => a + Math.random() * (b - a);
-  const easeIn    = t => t * t * t;
-  const easeOut   = t => 1 - Math.pow(1 - t, 3);
-  return { lerp, clamp, randRange, easeIn, easeOut };
-})();
